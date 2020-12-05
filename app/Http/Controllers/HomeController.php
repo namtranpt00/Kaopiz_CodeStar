261439d93afe83fd29846ad75cc1abaa58ab1fb5
namtranpt00 <namtranpt00@users.noreply.github.com>
2020-12-06 00:30:38 +0700

    tak1_2_6/12/20

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+
+    public function index($name = "")
+    {
+        return "homeController" . $name;
+    }
+
+    public function task1()
+    {
+        return view('task1', ['title' => 'Homework_task1']);
+    }
+
+    public function task2Form(Request $request)
+    {
+        if(!isset($request->slug)){
+            return view('task2');
+        }
+        if(!isset($request->title)){
+            return view('task2');
+        }
+        if(!isset($request->contents)){
+            return view('task2');
+        }
+        return view('task2Display')->with(['slug'=>$request->slug,'title'=>$request->title,'content'=>$request->contents]);
+
+
+
+    }
+    public function task2Display(Request $request)
+    {
+        return view('task2Display');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,44 @@
+<?php
+
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('home', function () {
+    return "Home Page";
+});
+Route::get('login', function () {
+    return "loginPage";
+});
+Route::get('task1', [HomeController::class, 'task1'])->name('homework.task1');
+Route::get('task2', [HomeController::class, 'task2Form'])->name('homework.task2Form');
+Route::get('task2/display', [HomeController::class, 'task2Display'])->name('homework.task2Display');
+//Route::get('/welcome/', function () {
+//    return view('posts.index', ['name' => 'nam']);
+//});
+//Route::get('/home/{name?}', [HomeController::class, 'index']);
+//Route::get('/demo', [HomeController::class, 'demo']);
+//Route::get('/anotherDemo', [HomeController::class, 'anotherDemo']);
+//Route::get('/demo1', function (){
+//    return response('hello world',200)->header('content-type', 'text/plain');
+//}) ;
+//Route::get('posts',[PostController::class, 'index'])->name('posts.index');
+//Route::get('/task', [HomeController::class, 'task1']);
+
+
+
+
